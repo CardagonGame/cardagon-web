f48963253b4dd159920ec546a0c41df45842ac6f
moisout <maurice@oeger.li>
2025-11-22 14:40:46 +0100

    Add toaster

--- packages/cardagon-web/src/routes/__root.tsx
+++ packages/cardagon-web/src/routes/__root.tsx
@@ -2,10 +2,24 @@ import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { Toaster } from 'sonner'
 import '../index.css'
+import '../styles/toaster.css'
 
 const RootLayout = () => (
   <div className="app-root">
-    <Toaster />
+    <Toaster
+      className="toaster"
+      toastOptions={{
+        duration: 100000,
+        classNames: {
+          toast: 'toast',
+          title: 'title',
+          description: 'description',
+          actionButton: 'action-button',
+          cancelButton: 'cancel-button',
+          closeButton: 'close-button',
+        },
+      }}
+    />
     <Outlet />
     <TanStackRouterDevtools />
   </div>
